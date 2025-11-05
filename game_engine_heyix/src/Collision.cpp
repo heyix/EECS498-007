ec69ed42e6ad95326a34d1a96bb02f8db3255d25
heyix <heyixu0@gmail.com>
2025-11-05 00:05:18 -0500

    计算多边形碰撞

--- game_engine_heyix/src/Collision.cpp
+++ game_engine_heyix/src/Collision.cpp
@@ -1,6 +1,7 @@
 #include "Collision.h"
 #include <iostream>
 #include <limits>
+#include <algorithm>
 namespace FlatPhysics {
 	bool FlatPhysics::Collision::IntersectCircles(Vector2 centerA, float radiusA, Vector2 centerB, float radiusB, Vector2* normal, float* depth)
 	{
@@ -19,21 +20,33 @@ namespace FlatPhysics {
 		return true;
 	}
 
-	bool Collision::IntersectPolygons(const std::vector<Vector2>& verticesA, const std::vector<Vector2>& verticesB)
+	bool Collision::IntersectPolygons(const std::vector<Vector2>& verticesA, const std::vector<Vector2>& verticesB, Vector2* normal, float* depth)
 	{
+		float min_depth = std::numeric_limits<float>::max();
+		Vector2 result_normal = Vector2::Zero();
 		for (int i = 0; i < verticesA.size(); i++) {
 			const Vector2& va = verticesA[i];
-			const Vector2& vb = verticesA[(i + 1)%verticesA.size()];
+			const Vector2& vb = verticesA[(i + 1) % verticesA.size()];
 
 			Vector2 edge = vb - va;
 			Vector2 axis = Vector2(-edge.y(), edge.x());
 
+			float axis_len = axis.Normalize();
+			if (axis_len <= 1e-8f) continue;
+
 			std::pair<float, float> projA = ProjectVertices(verticesA, axis);
 			std::pair<float, float> projB = ProjectVertices(verticesB, axis);
+
 			if (projA.first >= projB.second || projB.first >= projA.second) {
 				return false;
 			}
+			float axis_depth = std::min(projB.second - projA.first, projA.second - projB.first);
+			if (axis_depth < min_depth) {
+				min_depth = axis_depth;
+				result_normal = axis;
+			}
 		}
+
 		for (int i = 0; i < verticesB.size(); i++) {
 			const Vector2& va = verticesB[i];
 			const Vector2& vb = verticesB[(i + 1) % verticesB.size()];
@@ -41,15 +54,37 @@ namespace FlatPhysics {
 			Vector2 edge = vb - va;
 			Vector2 axis = Vector2(-edge.y(), edge.x());
 
+			float axis_len = axis.Normalize();
+			if (axis_len <= 1e-8f) continue;
+
 			std::pair<float, float> projA = ProjectVertices(verticesA, axis);
 			std::pair<float, float> projB = ProjectVertices(verticesB, axis);
+
 			if (projA.first >= projB.second || projB.first >= projA.second) {
 				return false;
 			}
+			float axis_depth = std::min(projB.second - projA.first, projA.second - projB.first);
+			if (axis_depth < min_depth) {
+				min_depth = axis_depth;
+				result_normal = axis; 
+			}
+		}
+		Vector2 centerA = FindPolygonArithmeticMean(verticesA);
+		Vector2 centerB = FindPolygonArithmeticMean(verticesB);
+		Vector2 direction = centerB - centerA;
+		if (Vector2::Dot(direction, result_normal) < 0) {
+			result_normal = -result_normal;
+		}
+		if (normal) {
+			*normal = result_normal;
+		}
+		if (depth) {
+			*depth = min_depth;
 		}
 		return true;
 	}
 
+	//return {min,max}
 	std::pair<float, float> Collision::ProjectVertices(const std::vector<Vector2>& vertices, Vector2 axis)
 	{
 #pragma warning(push)
@@ -69,6 +104,17 @@ namespace FlatPhysics {
 		return { min,max };
 
 #pragma warning(pop)
+	}
+
+	Vector2 Collision::FindPolygonArithmeticMean(const std::vector<Vector2>& vertices)
+	{
+		float sumX = 0;
+		float sumY = 0;
+		for (int i = 0; i < vertices.size(); i++) {
+			sumX += vertices[i].x();
+			sumY += vertices[i].y();
+		}
+		return { sumX / vertices.size(),sumY / vertices.size() };
 	}
 
 }
--- game_engine_heyix/src/Collision.h
+++ game_engine_heyix/src/Collision.h
@@ -5,9 +5,10 @@ namespace FlatPhysics {
 	class Collision {
 	public:
 		static bool IntersectCircles(Vector2 centerA, float radiusA, Vector2 centerB, float radiusB, Vector2* normal = nullptr, float* depth = nullptr);
-		static bool IntersectPolygons(const std::vector<Vector2>& verticesA, const std::vector<Vector2>& verticesB);
+		static bool IntersectPolygons(const std::vector<Vector2>& verticesA, const std::vector<Vector2>& verticesB, Vector2* normal = nullptr, float* depth = nullptr);
 
 	private:
-		static std::pair<float,float> ProjectVertices(const std::vector<Vector2>& vertices, Vector2 axis);
+		static std::pair<float, float> ProjectVertices(const std::vector<Vector2>& vertices, Vector2 axis);
+		static Vector2 FindPolygonArithmeticMean(const std::vector<Vector2>& vertices);
 	};
 }
