15d2065b178c3c5fa5a551e285e9da14e13fc143
heyix <heyixu0@gmail.com>
2025-11-23 22:03:45 -0500

    启用MPI

--- game_engine_heyix/src/main.cpp
+++ game_engine_heyix/src/main.cpp
@@ -6,16 +6,28 @@
 #define _CRTDBG_MAP_ALLOC
 #include <cstdlib>
 #include <crtdbg.h>
+#include <mpi.h>
+
 int main(int argc, char* argv[]) {
+	MPI_Init(&argc, &argv);
+
+	int world_size = 0;
+	int world_rank = 0;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 #ifdef _DEBUG
 	// Enable debug heap + auto leak check at program exit
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	//_CrtSetBreakAlloc(26541);
 #endif
-	omp_set_num_threads(8);
-	Engine engine;
-	std::unique_ptr<Game> game = std::make_unique<Game>();
-	engine.set_running_game(std::move(game));
-	engine.run_game();
+	//omp_set_num_threads(8);
+	//Engine engine;
+	//std::unique_ptr<Game> game = std::make_unique<Game>();
+	//engine.set_running_game(std::move(game));
+	//engine.run_game();
+	//return 0;
+
+
+	MPI_Finalize();
 	return 0;
 }
